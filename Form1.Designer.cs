18178356be5b7abd2db99597b04c6fad6ebee4c7
DrizzienTP <DrizzienTP@DESKTOP-JEEOPBA>
2022-03-30 09:18:19 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Balon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrDibujar = new System.Windows.Forms.Timer(this.components);
+            this.picImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrDibujar
+            // 
+            this.tmrDibujar.Enabled = true;
+            this.tmrDibujar.Interval = 1;
+            this.tmrDibujar.Tick += new System.EventHandler(this.tmrDibujar_Tick);
+            // 
+            // picImagen
+            // 
+            this.picImagen.Location = new System.Drawing.Point(276, 185);
+            this.picImagen.Name = "picImagen";
+            this.picImagen.Size = new System.Drawing.Size(125, 58);
+            this.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagen.TabIndex = 0;
+            this.picImagen.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picImagen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logo DVD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrDibujar;
+        private System.Windows.Forms.PictureBox picImagen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Balon
+{
+    public partial class Form1 : Form
+    {
+        // Parametors del dibujo
+        private  int BolaWidth = 128;
+        private  int BolaHeight = 58;
+        private int BolaX, BolaY;   //pos
+        private int BolaVX, BolaVY; // vel.
+        public string[] files;
+        private int currentIndex = 0;
+        int img = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            inizializarImagenes();
+            
+        }
+        private void cambiarImages()
+        {
+            picImagen.ImageLocation = files[currentIndex];
+        }
+        private void inizializarImagenes()
+        {
+            string appRoot = System.IO.Path.GetDirectoryName(Application.StartupPath);
+            files = System.IO.Directory.GetFiles(appRoot + @"\dvdLogos");
+            Random rnd = new Random();
+            files = files.OrderBy(x => rnd.Next()).ToArray();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picImagen.Size = new System.Drawing.Size(BolaWidth, BolaHeight);
+            Random rnd = new Random();
+         
+            BolaVX = 5;
+            BolaVY = 5;
+            BolaX = rnd.Next(0, ClientSize.Width - BolaWidth);
+            BolaY = rnd.Next(0, ClientSize.Height - BolaHeight);
+           
+        }
+        private void siguienteImagen() {
+            if (img >= files.Length)
+            {
+                img = 0;
+                currentIndex = 0;
+            }
+            currentIndex = img;
+            img++;
+            
+            cambiarImages();
+        }
+        private void tmrDibujar_Tick(object sender, EventArgs e)
+        {
+            BolaX += BolaVX;
+            if (BolaX < 0)
+            {
+                BolaVX = -BolaVX;
+                siguienteImagen();
+            }
+            else if (BolaX + BolaWidth > ClientSize.Width)
+            {
+                BolaVX = -BolaVX;
+                siguienteImagen();
+            }
+
+            BolaY += BolaVY;
+            if (BolaY < 0)
+            {
+                BolaVY = -BolaVY;
+                siguienteImagen();
+
+            }
+            else if (BolaY + BolaHeight > ClientSize.Height)
+            {
+                BolaVY = -BolaVY;
+                siguienteImagen();
+                
+            }
+
+            picImagen.Location = new Point(BolaX, BolaY);
+            Refresh();
+        }
+
+        
+    }
+}
